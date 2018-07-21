3a1953b3d085aa97892a921e729ffd5262602769
boatio <37399578+boatio@users.noreply.github.com>
2018-07-21 16:16:40 +0900

    Create boat64.js

---
+++ boat64.js
@@ -0,0 +1,18 @@
+const boat64 =
+{
+  "en":function(__s){
+        var __data = "";
+        var __array = [];
+        for(var i=0;i<__s.length;i++){
+        __array.push(__s.charCodeAt(i)*120-__s.length);
+ 	      __data += String.fromCharCode(__array[i]);
+
+
+
+  
+  },
+  "de":function(){
+
+  }
+  
+  };
